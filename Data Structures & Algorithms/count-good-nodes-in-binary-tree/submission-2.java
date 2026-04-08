c19cb5db8db9d9ed7fa390f0dbfdd5408a0916a2
Aashirbad Dhital <60020431+Aashirbadd@users.noreply.github.com>
2026-04-08 10:11:00 -0700

    Add: count-good-nodes-in-binary-tree - submission-2

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-2.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int goodNodes(TreeNode root) {
+        return dfs(root, Integer.MIN_VALUE);
+    }
+
+    private int dfs(TreeNode root, int maxInPath) {
+        if (root == null) return 0;
+
+        int nodeIsGood = 0;
+        // Good Node Case
+        if (root.val >= maxInPath) {
+            nodeIsGood = 1;
+            maxInPath = root.val;
+        }
+
+        int left = dfs(root.left, maxInPath);
+        int right = dfs(root.right, maxInPath);
+
+        return left + right + nodeIsGood;
+    }
+}
+
+// Return the number of good nodes: A node (X) is considered GOOD if the path from the ROOT
+// of the tree to node (X) contains no values greater than node X.
+
+// Input: root = [2,1,1,3,null,1,5]
+//              [2]
+//            1 (2)    1 (2)
+//          [3](3) n  1[5] (2)
