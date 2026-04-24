676405faf1181cfbad776e6aa52021584ab2c48a
Aashirbad Dhital <60020431+Aashirbadd@users.noreply.github.com>
2026-04-24 11:43:47 -0700

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,58 @@
+class Solution {
+    // Memoised DFS solution, then bottom up solution.
+    public int climbStairs(int n) {
+        HashMap<Integer, Integer> cache = new HashMap<>();
+
+        // dfs from step 0 to step n
+        return dfs(0, n, 0, cache);
+    }
+
+    public int dfs(int curr, int goal, int currStepsTaken, HashMap<Integer, Integer> cache) {
+        if (curr > goal)                return 0;
+        // Base case, == 5
+        if (curr == goal)               return 1;
+        // Base case (already in cache)
+        if (cache.containsKey(curr))    return cache.get(curr);
+
+        // The increment cases
+        int oneStep = dfs(curr + 1, goal, currStepsTaken + 1, cache);
+        cache.put(curr + 1, oneStep);
+        int twoStep = dfs(curr + 2, goal, currStepsTaken + 1, cache);
+        cache.put(curr + 2, twoStep);
+
+        return oneStep + twoStep;
+    }
+}
+
+// Input: int n ~ # of steps to reach the top of the staircase.
+
+// Constraints:
+// You can climb either 1 or 2 steps at a time. 
+
+// Goal:
+// Return the # of distinct ways to climb to the top of the staircase
+
+
+// Test cases:
+// Input: n = 2, Output: 2
+// Why?
+// 1) 1 + 1 = 2
+// 2) 2 = 2
+
+// Essentially, how many different ways can we get to a sum of target (if we're only adding 1's or 2's)
+
+// Input: n = 3 Output: 3
+
+// 1) 1 + 1 + 1 = 3
+// 2) 1 + 2 = 3
+// 3) 2 + 1 = 3
+
+// So I noticed that the answer builds on the previous numbers aswell.
+
+// Input: n = 4 Output: 5
+
+// 1) 1 + 1 + 1 + 1 = 4
+// 2) 1 + 1 + 2 = 4
+// 3) 1 + 2 + 1 = 4
+// 4) 2 + 1 + 1 = 4
+// 5) 2 + 2 = 4
